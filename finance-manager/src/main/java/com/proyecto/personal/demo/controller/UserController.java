2e839c817cb0eb19ce62c740c12e6741cbc9acb6
wigek <wilder.garciaramirez@gmail.com>
2026-07-21 16:06:26 -0500

    feat: creación usercontroller

---
+++ finance-manager/src/main/java/com/proyecto/personal/demo/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.proyecto.personal.demo.controller;
+
+import com.proyecto.personal.demo.dto.UserRegisterRequest;
+import com.proyecto.personal.demo.entity.User;
+import com.proyecto.personal.demo.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody UserRegisterRequest request) {
+        User createdUser = userService.register(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+}
